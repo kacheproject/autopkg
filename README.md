b316b8d21be31643f7d3c624402fef9b34380ef8
thisLight <l1589002388@gmail.com>
2021-09-12 10:02:24 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Autopkg
+Autopkg is a library works with zig build system to simplify package management.
+
+Featured:
+- Declrative experience
+- Programable package
+- Nested directory structure support
+- Working with C headers and sources
+
+Zig build system lack of these important QoL features. This library is intented to provide them.
+
+## Basic Usage
+
+This library is zero-dependency. Just copy the `autopkg.zig` or refer by git submodule.
+
+In the `build.zig` of the package `./package` you need to work with:
+````zig
+const autopkg = @import("autopkg/autopkg.zig");
+
+/// `name` is the name you name.
+/// `path` is relative path to this library directory.
+pub fn package(name: []const u8, path: []const u8) autopkg.AutoPkgI {
+    return autopkg.genExport(.{
+        .name = name,
+        .path = path,
+        .rootSrc = "src/main.zig", // Use empty string if you don't have zig source file.
+        // You can specify more options about libraries and C sources, see source code autopkg.zig (don't worry, the code is short). All options works locally.
+    });
+}
+````
+
+In the `build.zig` in your main package `.`:
+````zig
+const std = @import("std");
+const autopkg = @import("autopkg/autopkg.zig");
+
+pub fn package(name: []const u8, path: []const u8) autopkg.AutoPkgI {
+    const thePackageYouNeed = @import("./package/build.zig");
+    return autopkg.genExport(.{
+        .name = name,
+        .path = path,
+        .rootSrc = "src/main.zig",
+        .dependencies = &.{
+            autopkg.accept(thePackageYouNeed.package("package", "./package")),
+        },
+    });
+}
+
+pub fn build(b: *std.build.Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    const mainPackage = autopkg.accept(package("main", "."));
+    var resolvedPackage = mainPackage.resolve(".", b.allocator) catch unreachable;
+    const lib = resolvedPackage.addBuild(b);
+    lib.setBuildMode(mode);
+    lib.install();
+}
+````
+
+The `package()` function is not limited to the two parameters specified here. You can return alternative version of `AutoPkgI` depends on the arguments or even the environment.
+
+If you don't want to specify a auto package and just want to depend on one of using that, you could use `AutoPkg.dependedBy()`:
+
+````zig
+const thePackageYouWorkWith = @import("./package/build.zig");
+const autopkg = @import("autopkg/autopkg.zig");
+
+pub fn build(b: *std.build.Builder) void {
+    const mode = b.standardReleaseOptions();
+
+    const resolvedThePackage = autopkg.accept(thePackageYouWorkWith.package("package", "./package")).resolve(".", b.allocator) catch unreachable; // You need to resolve the package first
+    const lib = b.addStaticLibrary("mypackage", "src/main.zig");
+    resolvedThePackage.dependedBy(lib);
+    lib.setBuildMode(mode);
+    lib.install();
+}
+````
+
+## Contributing
+
+Suggestion/Pull Request welcome!
+
+### Maintainer
+
+`Rubicon Rowe <l1589002388 & gmail.com>`
+
+## License
+`Apache-2.0`.
---
+++ autopkg.zig
@@ -0,0 +1,200 @@
+// Copyright 2021 Rubicon Rowe.
+// SPDX-License-Identifier: Apache-2.0
+
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const _p = std.fs.path;
+
+fn PackedSlice (comptime T: type, constant: bool) type {
+    const Type = if (constant) [*]const T else [*] T;
+    return packed struct {
+        ptr: Type,
+        len: usize,
+
+        const Self = @This();
+
+        pub fn init(s: if (constant) []const T else []T) Self {
+            return Self {
+                .ptr = s.ptr,
+                .len = s.len,
+            };
+        }
+
+        pub fn slice(self: *Self) if (constant) []const T else []T {
+            return self.ptr[0..self.len];
+        }
+    };
+}
+
+/// This is a travsal structure for your package.
+/// The structure is used to transfer infomation between different autopkg in different import container.
+pub const AutoPkgI = packed struct {
+    name: PackedSlice(u8, true),
+    path: PackedSlice(u8, true),
+    rootSrc: PackedSlice(u8, true), // Releative to .path
+    dependencies: PackedSlice(AutoPkg, false) = undefined,
+    includeDirs: PackedSlice([]const u8, true) = undefined,
+    cSrcFiles: PackedSlice([]const u8, true) = undefined,
+    ccflags: PackedSlice([]const u8, true) = undefined,
+    linkLibC: bool = false,
+    alloc: usize = 0,
+    linkSystemLibs: PackedSlice([]const u8, true) = undefined,
+    linkLibNames: PackedSlice([]const u8, true) = undefined,
+    libraryPaths: PackedSlice([]const u8, true) = undefined,
+
+    const Self = @This();
+
+    pub fn fromNormal(val: AutoPkg) Self {
+        return AutoPkgI {
+            .name = PackedSlice(u8, true).init(val.name),
+            .path = PackedSlice(u8, true).init(val.path),
+            .rootSrc = PackedSlice(u8, true).init(val.rootSrc),
+            .dependencies = PackedSlice(AutoPkg, false).init(val.dependencies),
+            .includeDirs = PackedSlice([]const u8, true).init(val.includeDirs),
+            .cSrcFiles = PackedSlice([]const u8, true).init(val.cSrcFiles),
+            .ccflags = PackedSlice([]const u8, true).init(val.ccflags),
+            .linkLibC = val.linkLibC,
+            .alloc = if (val.alloc) |alloc| @ptrToInt(alloc) else 0,
+            .linkSystemLibs = PackedSlice([]const u8, true).init(&.{}),
+            .linkLibNames = PackedSlice([]const u8, true).init(&.{}),
+            .libraryPaths = PackedSlice([]const u8, true).init(&.{}),
+        };
+    }
+
+    pub fn toNormal(self: *Self) AutoPkg {
+        return AutoPkg {
+            .name = self.name.slice(),
+            .path = self.path.slice(),
+            .rootSrc = self.rootSrc.slice(),
+            .dependencies = self.dependencies.slice(),
+            .includeDirs = self.includeDirs.slice(),
+            .cSrcFiles = self.cSrcFiles.slice(),
+            .ccflags = self.ccflags.slice(),
+            .linkLibC = self.linkLibC,
+            .alloc = if (self.alloc != 0) @intToPtr(*Allocator, self.alloc) else null,
+        };
+    }
+};
+
+pub fn accept(pkg: anytype) AutoPkg {
+    var casted = @bitCast(AutoPkgI, pkg);
+    return casted.toNormal();
+}
+
+pub fn genExport(pkg: AutoPkg) AutoPkgI {
+    return AutoPkgI.fromNormal(pkg);
+}
+
+/// Declare your package.
+/// `name` is the name.
+/// `path` is relative path to this library from parent AutoPkg declaration or the file where call .addBuild().
+/// `rootSrc` could be "" to tell zig build system building without zig file.
+/// `dependencies` is dependencies of this package.
+pub const AutoPkg = struct {
+    name: []const u8,
+    path: []const u8,
+    rootSrc: []const u8, // Releative to .path
+    dependencies: []AutoPkg = &.{},
+    includeDirs: []const []const u8 = &.{},
+    cSrcFiles: []const []const u8 = &.{},
+    ccflags: []const []const u8 = &.{},
+    linkLibC: bool = false,
+    alloc: ?*Allocator = null,
+
+    const Self = @This();
+
+    /// Add to builder as a static library.
+    pub fn addBuild(self: *const Self, b: *std.build.Builder) *std.build.LibExeObjStep {
+        var dependedSteps = b.allocator.alloc(*std.build.LibExeObjStep, self.dependencies.len) catch unreachable;
+        defer b.allocator.free(dependedSteps);
+        for (self.dependencies) |d, i| {
+            dependedSteps[i] = d.addBuild(b);
+        }
+        var me = b.addStaticLibrary(self.name, if (self.rootSrc.len != 0) self.rootSrc else null);
+        for (dependedSteps) |step| {
+            me.linkLibrary(step);
+        }
+        for (self.includeDirs) |dir| {
+            me.addIncludeDir(dir);
+        }
+        for (self.cSrcFiles) |file| {
+            me.addCSourceFile(file, self.ccflags);
+        }
+        if (self.linkLibC) {
+            me.linkLibC();
+        }
+        return me;
+    }
+
+    pub fn dependedBy(self: *const Self, step: *std.build.LibExeObjStep) void {
+        var buildStep = self.addBuild(step.builder);
+        step.linkLibrary(buildStep);
+    }
+
+    /// Resolve all pathes, this method should not be called twice or more.
+    pub fn resolve(self: *const Self, basePath: []const u8, alloc: *Allocator) Allocator.Error!AutoPkg {
+        const rootPathVec = &.{basePath, self.path};
+        var rootPath = try _p.join(alloc, rootPathVec);
+        // (Rubicon:) above line was "var rootPath = try _p.join(alloc, &.{basePath, self.path});"
+        // , but I got a segfault in zig 0.8.0-600 (Fedora 34 built) without compiling error.
+        // Add std.debug.printf("{s}/{s}", .{basePath, self.path}); before the line then works fine.
+        // Could it caused by the missing data doesn't be stored in stack and be refered
+        // when directly used as &.{} in function argument? Might be a compiler bug, IDK.
+        // Use the original version (it's more clear) if that were fixed.
+        errdefer alloc.free(rootPath);
+
+        var newDependencies = try alloc.alloc(AutoPkg, self.dependencies.len);
+        errdefer alloc.free(newDependencies);
+        for (self.dependencies) |d, i| {
+            newDependencies[i] = try d.resolve(rootPath, alloc);
+            errdefer d.deinit();
+        }
+
+        var newIncludeDirs = try alloc.alloc([]const u8, self.includeDirs.len);
+        errdefer alloc.free(newIncludeDirs);
+        for (self.includeDirs) |dir, i| {
+            newIncludeDirs[i] = try _p.join(alloc, &.{rootPath, dir});
+            errdefer alloc.free(newIncludeDirs[i]);
+        }
+
+        var newCSrcFiles = try alloc.alloc([]const u8, self.cSrcFiles.len);
+        errdefer alloc.free(newCSrcFiles);
+        for (self.cSrcFiles) |f, i| {
+            newCSrcFiles[i] = try _p.join(alloc, &.{rootPath, f});
+            errdefer alloc.free(newCSrcFiles[i]);
+        }
+        
+        var newRootSrc = if (self.rootSrc.len != 0) try _p.join(alloc, &.{rootPath, self.rootSrc}) else try alloc.dupe(u8, self.rootSrc);
+        errdefer alloc.free(newRootSrc);
+
+        return AutoPkg {
+            .name = try alloc.dupe(u8, self.name),
+            .path = rootPath,
+            .rootSrc = newRootSrc,
+            .dependencies = newDependencies,
+            .includeDirs = newIncludeDirs,
+            .cSrcFiles = newCSrcFiles,
+            .ccflags = self.ccflags,
+            .linkLibC = self.linkLibC,
+            .alloc = alloc,
+        };
+    }
+
+    pub fn deinit(self: *Self) void {
+        for (self.dependencies) |d| {
+            d.deinit();
+        }
+        if (self.alloc) |alloc| {
+            alloc.free(self.name);
+            alloc.free(self.path);
+            alloc.free(self.rootSrc);
+            inline for (.{self.dependencies, self.includeDirs, self.cSrcFiles}) |arr| {
+                inline for (arr) |e| {
+                    alloc.free(e);
+                }
+                alloc.free(arr);
+            }
+            self.alloc = null;
+        }
+    }
+};
